caa33ef3353939d1baab88748c663488809e1688
노승은 <noseun101@gmail.com>
2021-10-04 12:14:00 +0900

    스타벅스 Web: Badge Scroll

--- js/main.js
+++ js/main.js
@@ -20,3 +20,28 @@ searchInputEl.addEventListener('blur', function() {
 });
 
 // 오류 해결: remove를 안하고 blur를 하였더니 검색란이 blur되지 않았음
+
+
+const badgeEl = document.querySelector('header .badges');
+
+
+// 화면 자체에 스크롤을 추가하면 해당 함수를 출력한다.
+// 스크롤 낭비 절감: lodash의 throttle() 이용하여 스크롤 제한함.
+window.addEventListener('scroll', _.throttle(function() {
+  console.log(window.scrollY);
+  if(window.scrollY > 500) {    
+    // badge 숨김
+    // gsap.to(요소, 지속 시간(sec) , 옵션);
+    gsap.to(badgeEl, .6, {
+      opacity: 0,   // 점차 투명해짐
+      display: 'none'   // 실제로 요소 안보이게 함
+    });
+  } else {
+    // badge 보임
+    gsap.to(badgeEl, .6, {
+      opacity: 1,   // 점차 투명해짐
+      display: 'block'
+    });
+  }
+}, 300));
+// _.throttle(함수, 시간(msec)) 추가. 
